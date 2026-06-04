4da32136ac511952eee44ba43b5a2fe20217d336
Rofhiwa - Analyst <muthivhi0207@gmail.com>
2026-06-04 20:30:12 +0200

    Rename from sales_extract ro sales_export and also adding the stores_export script

---
+++ 02_sql/phase_05_exports/stores_export.sql
@@ -0,0 +1,7 @@
+-- Exporting the final Stores table to result as a stores.csv
+
+-- =======================================================
+-- Retireve all the columns and rows from the Stores table
+-- =======================================================
+SELECT *
+FROM [dbo].[Stores];
