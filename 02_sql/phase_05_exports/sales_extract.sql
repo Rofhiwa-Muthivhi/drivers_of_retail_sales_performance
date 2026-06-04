e221724df64b02636e1c91ee044f4e5638590add
Rofhiwa - Analyst <muthivhi0207@gmail.com>
2026-06-04 20:25:37 +0200

    Created and adding the sales extracting script

---
+++ 02_sql/phase_05_exports/sales_extract.sql
@@ -0,0 +1,7 @@
+-- Exporting the final Sales table to result as a sales.csv
+
+-- =======================================================
+-- Retireve all the columns and rows from the sales table
+-- =======================================================
+SELECT *
+FROM [dbo].[Sales];
