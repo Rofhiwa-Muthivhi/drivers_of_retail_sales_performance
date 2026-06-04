7ff48f38d3ad34383045baa64da3e0c8c1199e8c
Rofhiwa - Analyst <muthivhi0207@gmail.com>
2026-06-04 20:34:02 +0200

    Adding the features_export script

---
+++ 02_sql/phase_05_exports/features_export.sql
@@ -0,0 +1,7 @@
+-- Exporting the final Features table to result as a Features.csv
+
+-- =======================================================
+-- Retireve all the columns and rows from the Features table
+-- =======================================================
+SELECT *
+FROM [dbo].[Features];
