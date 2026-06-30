6df213425ae9043737a8950f80e9dbc76cc28b7e
Rofhiwa - Analyst <muthivhi0207@gmail.com>
2026-06-30 13:01:23 +0200

    query restructure for readability and comments refine

--- 02_sql/phase_03_data_cleaning/05_join_validation.sql
+++ 02_sql/phase_03_data_cleaning/05_join_validation.sql
@@ -5,8 +5,7 @@
 -- Verifies that sales records matches store and features data.
 -- INNER JOIN is used because it only keeps matches.
 -- ========================================================================================================
-SELECT 
-      COUNT(*) AS total_rows
+SELECT COUNT(*) AS total_rows
 FROM sales s
 INNER JOIN stores st
 ON s.store = st.store
@@ -16,13 +15,12 @@ ON s.store = f.store AND s.date = f.date;
 
 -- ===================================================================================================
 -- We want to identify any sales records that do not have matches with store or feature records.
--- This matters because e can find missing relationships between datasets
+-- This matters because we can find missing relationships between datasets
 -- The 0 missing matches confirms that all sales records connects to store and features data
 -- ===================================================================================================
 
 
-SELECT 
-      COUNT(*) AS missing_matches
+SELECT COUNT(*) AS missing_matches
 FROM sales s
 LEFT JOIN stores st
 ON s.store = st.store
