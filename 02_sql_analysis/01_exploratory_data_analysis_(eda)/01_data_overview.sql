4f442366a1cbff9edc07b3315940685a9096db59
Rofhiwa - Analyst <muthivhi0207@gmail.com>
2026-07-27 11:53:00 +0200

    Data overview script add

---
+++ 02_sql_analysis/01_exploratory_data_analysis_(eda)/01_data_overview.sql
@@ -0,0 +1,13 @@
+-- Data overview: provides a high level view of each table.
+
+-- Sales tables overview.
+SELECT *
+FROM [retail_analysis].[dbo].[sales];
+
+--	Stores table overview.
+SELECT *
+FROM [retail_analysis].[dbo].[stores];
+
+-- Features table overview.
+SELECT *
+FROM [retail_analysis].[dbo].[features];
